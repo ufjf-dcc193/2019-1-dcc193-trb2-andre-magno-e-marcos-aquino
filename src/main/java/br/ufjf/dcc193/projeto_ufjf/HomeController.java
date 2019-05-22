e2542277502e96bda8f49c41b98dec2a8e0c37cd
andremagnoribeiro <andremagnoribeiro@gmail.com>
2019-05-22 20:26:29 -0300

    criação do projeto

---
+++ src/main/java/br/ufjf/dcc193/projeto_ufjf/HomeController.java
@@ -0,0 +1,16 @@
+package br.ufjf.dcc193.projeto_ufjf;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    @ResponseBody
+    public String index(){
+        return "<h1>Bem vindo !!</h1>";
+    }
+}
