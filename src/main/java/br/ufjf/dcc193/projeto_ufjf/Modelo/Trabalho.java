0f9e17a25bf12b75bb4e9423241a8041bb51f9f5
Marcos Aquino <marcos-aquino@live.com>
2019-06-04 16:49:31 -0300

    Criação da classe Trabalho

---
+++ src/main/java/br/ufjf/dcc193/projeto_ufjf/Modelo/Trabalho.java
@@ -0,0 +1,71 @@
+package br.ufjf.dcc193.projeto_ufjf.Modelo;
+
+import java.util.List;
+
+public class Trabalho {
+
+
+    private int id;
+
+    private String titulo;
+
+    private String descricao;
+
+    private String url;
+
+    private Area area;
+
+
+    public Trabalho(){
+
+    }
+
+    public Trabalho(String titulo, String descricao, String url, Area area){
+
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.url = url;
+        this.area = area;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Area getArea() {
+        return area;
+    }
+
+    public void setArea(Area area) {
+        this.area = area;
+    }    
+}
