b3b278a1852ec6c3b382f98d7a2e1d7459a5dfbd
Marcos Aquino <marcos-aquino@live.com>
2019-06-04 12:41:40 -0300

    Criação das classes Avaliador e Area

---
+++ src/main/java/br/ufjf/dcc193/projeto_ufjf/Modelo/Area.java
@@ -0,0 +1,35 @@
+package br.ufjf.dcc193.projeto_ufjf.Modelo;
+
+public class Area{
+
+    private int id;
+
+    private String descricao;
+
+    public Area(){
+
+    }
+
+    public Area(String descricao){
+        this.descricao = descricao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    
+
+}
---
+++ src/main/java/br/ufjf/dcc193/projeto_ufjf/Modelo/Avaliador.java
@@ -0,0 +1,71 @@
+import java.util.List;
+
+public class Avaliador {
+
+    private int id;
+
+    private int codigoAcesso;
+    
+    private String nomeCompleto;
+
+    private String email;
+
+    private List<Area> area;
+
+    public Avaliador(){
+
+    }
+
+    public Avaliador(int codigoAcesso, String nomeCompleto, String email){
+        this.codigoAcesso = codigoAcesso;
+        this.nomeCompleto = nomeCompleto;
+        this.email = email;
+        this.area = area;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCodigoAcesso() {
+        return codigoAcesso;
+    }
+
+    public void setCodigoAcesso(int codigoAcesso) {
+        this.codigoAcesso = codigoAcesso;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Area> getArea() {
+        return area;
+    }
+
+    public void setArea(List<Area> areaConhecimento) {
+        this.area = area;
+    }
+
+    
+
+    
+
+
+}
