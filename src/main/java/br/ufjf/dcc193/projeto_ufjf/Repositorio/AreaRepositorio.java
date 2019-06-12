eb9a7ed24f30e6d42fd45970dec488cccc955e7f
andremagnoribeiro <andremagnoribeiro@gmail.com>
2019-06-12 20:04:31 -0300

    modificações

--- src/main/java/br/ufjf/dcc193/projeto_ufjf/Repositorio/AreaRepositorio.java
+++
@@ -1,9 +0,0 @@
-package br.ufjf.dcc193.projeto_ufjf.Repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.ufjf.dcc193.projeto_ufjf.Modelo.Area;
-
-public interface AreaRepositorio extends JpaRepository<Area, Long>{
-
-}
